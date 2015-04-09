7ccc74a8ee31c55e8e2b8e789e7fb765019d55b5
YJ Kim <yjkimjunior@berkeley.edu>
2015-04-08 19:29:42 -0700

    styled sign_up and sign_in and added a browse button to nav bar

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+	
 	def index
 		@recipe = Recipe.all.order("created_at DESC")
 	end
